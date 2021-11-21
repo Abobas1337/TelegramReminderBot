7b902c36a0f552b9bbaec607f351ba60eda6c241
Abobas1337 <Kor.Ol2005@yandex.ru>
2021-11-21 17:28:05 +0300

    v0.0.1

--- my_telegram.py
+++ my_telegram.py
@@ -240,7 +240,7 @@ def job():
 
 def do_schedule():
 	print("Sheduling...")
-	schedule.every().day.at("17:27:00").do(job)
+	schedule.every().day.at("17:35:00").do(job)
 	while True:
 		schedule.run_pending()
 		time.sleep(1)
